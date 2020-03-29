62fafd7aad4137fbf2e73a5f2510f68b85b1b411
aurel fontaine <ajfame11@gmail.com>
2020-03-29 20:18:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  elements = ["earth", "fire", "water", "wind"]
+  array = ["earth", "fire", "water", "wind"]
 end
 
 def add_element_to_end_of_array(array, element)
-  elements << "lighting"
+  array << "lighting"
 end
 
 def add_element_to_start_of_array(array, element)
