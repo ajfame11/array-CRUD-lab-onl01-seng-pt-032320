40035e45efb5d7d3c60aeffd3136695dc69680df
aurel fontaine <ajfame11@gmail.com>
2020-03-29 20:17:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  Elements << "lighting"
+  elements << "lighting"
 end
 
 def add_element_to_start_of_array(array, element)
